3cda56dd0d5fb776fde914fbf2cc085fcdbf445f
ReWriteGame <mr.ekstrimal1432@gmail.com>
2021-08-13 13:36:15 +0300

    add button prefab. cannels win/lose. fix bug lvlController

--- Assets/Scripts/Architecture/LevelController.cs
+++ Assets/Scripts/Architecture/LevelController.cs
@@ -5,48 +5,55 @@ using UnityEngine.SceneManagement;
 public class LevelController : ScriptableObject
 {
     [SerializeField] private int mainSceneIndex = 0;
+    [SerializeField] private bool showConsoleMessage = true;
+
     public void loadNextLevel()
     {
+        resumeGame();
         int numberOfLevel = SceneManager.GetActiveScene().buildIndex;// get current level index
-        Debug.Log($"Load level index:{numberOfLevel + 1}");
+        if (showConsoleMessage) Debug.Log($"Load level index: {numberOfLevel + 1}");
 
         if (numberOfLevel < SceneManager.sceneCountInBuildSettings - 1)
             SceneManager.LoadScene(++numberOfLevel);// загрузка след уровня номер можно посмотреть через shift + ctrl + b
-        else Debug.Log($"Can't load next level. Level is last!");
+        else if (showConsoleMessage) Debug.LogWarning($"Can't load next level. Level is last!");
     }
 
     public void reStartLevel()
     {
+        resumeGame();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Debug.Log($"Level reloaded!");
+        if (showConsoleMessage) Debug.Log($"Level reloaded!");
     }
 
     public void loadLevel(int numLevel)
     {
-        Debug.Log($"Load level index:{numLevel}");
+        resumeGame();
+        Debug.Log($"Load level index: {numLevel}");
         if (numLevel < SceneManager.sceneCountInBuildSettings - 1 && numLevel >= 0)
             SceneManager.LoadScene(numLevel);
-        else Debug.Log($"Can't load level. Incorrect number of Level!");
+        else if (showConsoleMessage) Debug.LogWarning($"Can't load level. Incorrect number of Level!");
     }
 
     public void exitGame()
     {
+        resumeGame();
         Application.Quit();
-        Debug.Log("Quit the Game.");
+        if (showConsoleMessage) Debug.Log("Quit the Game.");
     }
     public void loadMainMenu()
     {
+        resumeGame();
         SceneManager.LoadScene(mainSceneIndex);
-        Debug.Log($"Load MainMenu scene index {mainSceneIndex}.");
+        if (showConsoleMessage) Debug.Log($"Load MainMenu scene index: {mainSceneIndex}.");
     }
     public void pauseGame()
     {
         Time.timeScale = 0;
-        Debug.Log($"Game paused.");
+        if (showConsoleMessage) Debug.Log($"Game paused.");
     }
     public void resumeGame()
     {
         Time.timeScale = 1;
-        Debug.Log($"Game continue.");
+        if (showConsoleMessage) Debug.Log($"Game continue.");
     }
 }
